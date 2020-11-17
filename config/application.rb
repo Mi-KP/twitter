4b0a0f6631810cb6c0261516c87718150605a148
root <root@localhost.localdomain>
2020-11-17 23:29:28 +0900

    時刻表示を日本表記に修正（ロケールを日本、書式設定、Lメソッド）

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module SampleApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # 追加 アプリケーションのロケールを日本に設定
+    config.i18n.default_locale = :ja
   end
 end
